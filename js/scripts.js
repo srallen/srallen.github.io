93fe76108e8bc6eeeb155b1d875927a0b1c7bc8b
Sarah Allen <sarah.sra1@gmail.com>
2014-01-23 15:06:43 -0600

    Added active class to navbar

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   $('nav > ul').on('click', 'li', function(e) {
     e.preventDefault();
     var $this = $(this);
+    var list = $('nav > ul')
     var container = $('html, body');
     var origin = $this.find('a').attr('name');
     var destination = $('#' + origin);
+
+    list.children().removeClass('active')
+    $this.addClass('active')
     container.animate({
       scrollTop: $(destination).offset().top - 140
     }, 1500)
